e2456516bc8524b06bce91f30fe65fd379905bef
demianov-an <Alexm92@inbox.ru>
2023-11-12 15:47:24 +0600

    monday-homework-2

---
+++ src/components/Profile.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img
+          src="https://mywowo.net/media/images/cache/dubai_img_worlds_of_adventure_01_presentazione_jpg_1200_630_cover_85.jpg"
+          alt=""/>
+      </div>
+      <div>
+        ava + description
+      </div>
+      <div>
+        My post
+        <div>
+          New post
+        </div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
