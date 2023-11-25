d911fea401c4e432d4822b3f2a461903737e38ad
demianov-an <Alexm92@inbox.ru>
2023-11-25 20:37:17 +0600

    lesoon 17-18

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -3,17 +3,20 @@ import s from './Post.module.css'
 
 type PostType = {
   message: string
+  likesCount: number
 }
 
 const Post: React.FC<PostType> = (props) => {
   return (
-        <div className={s.item}>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRf8ZaLUTA2vEGXFAah4FD0Xvpt7kK3l5gmhMDvPcQG6qu65IuvVJxi3_OofRKdB0AvP4Y&usqp=CAU" alt=""/>
-          {props.message}
-          <div>
-            <span>like</span>
-          </div>
-        </div>
+    <div className={s.item}>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRf8ZaLUTA2vEGXFAah4FD0Xvpt7kK3l5gmhMDvPcQG6qu65IuvVJxi3_OofRKdB0AvP4Y&usqp=CAU"
+        alt=""/>
+      {props.message}
+      <div>
+        <span>like</span> {props.likesCount}
+      </div>
+    </div>
   );
 };
 
