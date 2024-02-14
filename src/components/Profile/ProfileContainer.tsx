f20540157eaddf2126e40a9f49f3077e1977db13
demianov-an <alexm92@inbox.ru>
2024-02-14 20:12:22 +0600

    60 урок: Типизация withrouter

--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -5,13 +5,12 @@ import {UsersResponse} from "../Users/UsersContainer"
 import {connect} from "react-redux"
 import {ProfileType, setUserProfile} from "../../redux/profile-reducer"
 import {AppRootStateType} from "../../redux/redux-store"
-import {withRouter} from "react-router-dom"
+import {RouteComponentProps, withRouter} from "react-router-dom"
 
 class ProfileContainer extends React.Component<ProfilePropsType, any> {
   componentDidMount() {
-    // @ts-ignore
     let userId = this.props.match.params.userId
-    if (!userId) userId = 2
+    if (!userId) userId = '2'
     axios.get<UsersResponse>(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
       .then(response => {
         this.props.setUserProfile(response.data)
@@ -26,19 +25,25 @@ class ProfileContainer extends React.Component<ProfilePropsType, any> {
   }
 }
 
-export type ProfilePropsType = mapStatePropsType & mapDispatchPropsType
-
 type mapStatePropsType = {
   profile: ProfileType | null
 }
+
 type mapDispatchPropsType = {
   setUserProfile: (profile: any) => void
 }
+type PathParamsType = {
+  userId: string,
+}
+
+type OwnPropsType = mapStatePropsType & mapDispatchPropsType
+
+export type ProfilePropsType = RouteComponentProps<PathParamsType> & OwnPropsType
 
 let mapStateToProps = (state: AppRootStateType) => ({
   profile: state.profilePage.profile
 })
 
-const WithUrlDataContainerComponent = withRouter<any, any>(ProfileContainer)
+const WithUrlDataContainerComponent = withRouter(ProfileContainer)
 
 export default connect(mapStateToProps, {setUserProfile})(WithUrlDataContainerComponent)
